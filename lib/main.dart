3a89fa2e12dbd1cbb8ceef556b2a935581f2510c
ankit-apk <ankitsrivastava858@gmail.com>
2021-07-22 21:23:41 +0530

    Image link changed

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      
+      debugShowCheckedModeBanner: false,
       home: SplashScreenPage(),
     );
   }
